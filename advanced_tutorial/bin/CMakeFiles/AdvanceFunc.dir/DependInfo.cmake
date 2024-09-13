8afa04ec93c7cf25b90457066534b96c6f8ff714
SuzukiCCY <578229323@qq.com>
2024-09-14 00:10:48 +0800

    first commit

---
+++ advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ccy/Code/Cpp/advanced_tutorial/src/dynamic.cpp" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/dynamic.cpp.o" "gcc" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/dynamic.cpp.o.d"
+  "/Users/ccy/Code/Cpp/advanced_tutorial/src/exception.cpp" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/exception.cpp.o" "gcc" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/exception.cpp.o.d"
+  "/Users/ccy/Code/Cpp/advanced_tutorial/src/file_stream.cpp" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/file_stream.cpp.o" "gcc" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/file_stream.cpp.o.d"
+  "/Users/ccy/Code/Cpp/advanced_tutorial/src/multi-thread.cpp" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/multi-thread.cpp.o" "gcc" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/multi-thread.cpp.o.d"
+  "/Users/ccy/Code/Cpp/advanced_tutorial/src/namespace.cpp" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/namespace.cpp.o" "gcc" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/namespace.cpp.o.d"
+  "/Users/ccy/Code/Cpp/advanced_tutorial/src/preprocessor.cpp" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/preprocessor.cpp.o" "gcc" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/preprocessor.cpp.o.d"
+  "/Users/ccy/Code/Cpp/advanced_tutorial/src/signal_handle.cpp" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/signal_handle.cpp.o" "gcc" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/signal_handle.cpp.o.d"
+  "/Users/ccy/Code/Cpp/advanced_tutorial/src/stl.cpp" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/stl.cpp.o" "gcc" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/stl.cpp.o.d"
+  "/Users/ccy/Code/Cpp/advanced_tutorial/src/template.cpp" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/template.cpp.o" "gcc" "/Users/ccy/Code/Cpp/advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/template.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ advanced_tutorial/bin/CMakeFiles/AdvanceFunc.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AdvanceFunc.
+# This may be replaced when dependencies are built.
