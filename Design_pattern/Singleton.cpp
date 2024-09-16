fe872f30e95b19aa7933e78093e70993811dcaae
SuzukiCCY <578229323@qq.com>
2024-09-17 03:47:28 +0800

    add singleton

---
+++ Design_pattern/Singleton.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// 单例模式
+class A
+{
+// static函数可以直接通过类名调用，不需要创建对象
+// static函数内部不能使用非static成员变量和非static成员函数
+// static函数内部不能使用this指针
+public:
+    static A& getInstance();
+    int getval() const { return val; };
+private:
+    int val;
+    A(): val(0) {};
+    A(const A& val) = delete; // 禁止拷贝构造
+};
+
+A& A::getInstance(){
+    static A a;
+    return a;
+};
+
+int main()
+{   
+    cout << A::getInstance().getval() << endl;
+    return 0;
+}
