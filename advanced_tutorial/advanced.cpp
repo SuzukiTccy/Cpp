469601bc654940e2c3ec92404e2016423c578a77
SuzukiCCY <578229323@qq.com>
2025-10-19 20:45:48 +0800

    add some comment in advanced.cpp

--- advanced_tutorial/advanced.cpp
+++ advanced_tutorial/advanced.cpp
@@ -10,36 +10,43 @@ int main(){
 /*=======================file and stream========================*/
     cout << "<===========file and stream===========>" << endl;
     openFileTest();
+    cout << endl;
 
 
 /*=======================exception handing========================*/
     cout << "<===========exception handing===========>" << endl;
     exceptionTest();
+    cout << endl;
 
 
 /*=======================dynamic memory space========================*/
     cout << "<===========dynamic memory space===========>" << endl;
     dynamicTest();
+    cout << endl;
 
 
 /*=======================namespace========================*/
     cout << "<===========namespace===========>" << endl;
     namespaceTest();
+    cout << endl;
     
 
 /*=======================template========================*/
     cout << "<===========template===========>" << endl;
     templateTest();
+    cout << endl;
 
 
 /*=======================preprocessor========================*/
     cout << "<===========preprocessor===========>" << endl;
     preprocessorTest();
+    cout << endl;
 
 
 /*=======================signal Handle========================*/
     cout << "<===========signal Handle===========>" << endl;
     // signalHandleTest();
+    cout << endl;
 
 
 
@@ -47,15 +54,19 @@ int main(){
     cout << "<===========multi-thread===========>" << endl;
     cout << "<=====pthreadTest1()======>" << endl;
     // pthreadTest1();
+    cout << endl;
 
     cout << "<=====pthreadTest2()======>" << endl;
     // pthreadTest2();
+    cout << endl;
 
     cout << "<=====threadTest1()======>" << endl;
     // threadTest1();
+    cout << endl;
 
     cout << "<=====threadTest2()======>" << endl;
     threadTest2();
+    cout << endl;
 
 
 
@@ -63,6 +74,7 @@ int main(){
     cout << "<===========STL===========>" << endl;
     cout << "<===========vector===========>" << endl;
     vec();
+    cout << endl;
 
 
     return 0;
--- advanced_tutorial/src/preprocessor.cpp
+++ advanced_tutorial/src/preprocessor.cpp
@@ -10,31 +10,34 @@ using namespace std;
 
 void preprocessorTest(){
 
-#ifdef DEBUG
+    #ifdef DEBUG
 
-    cout << "<=========create the symbol constant=========>" << endl;
-    cout << "The value of PI is " << PI << endl; // usually call the 'PI' as macro
-
-
-    cout << "<=========macor with parameter=========>" << endl;
-    cout << "MAX(2,1) = " << MAX(2,1) << endl;
+        cout << "<=========create the symbol constant=========>" << endl;
+        cout << "The value of PI is " << PI << endl; // usually call the 'PI' as macro
+        cout << endl;
 
 
-    cout << "<=========# and ##=========>" << endl;
-    cout << "STR(444334) = " << STR(444334) << endl; //operator # convert into string
-    int ab = 100;
-    cout << "CONCAT(a,b) = " << CONCAT(a,b) << endl; // ## concat the 'a' and 'b', and the result is ab
-                                                     // so cout << CONCAT(a,b) == cout << ab
-    
+        cout << "<=========macor with parameter=========>" << endl;
+        cout << "MAX(2,1) = " << MAX(2,1) << endl;
+        cout << endl;
 
-    cout << "=========C++ predefined macro=========" << endl;
-    cout << "Value of __LINE__ : " << __LINE__ << endl;
-    cout << "Value of __FILE__ : " << __FILE__ << endl;
-    cout << "Value of __DATE__ : " << __DATE__ << endl;
-    cout << "Value of __TIME__ : " << __TIME__ << endl;  
 
-#endif
+        cout << "<=========# and ##=========>" << endl;
+        cout << "STR(444334) = " << STR(444334) << endl; //operator # convert into string
+        int ab = 100;
+        cout << "CONCAT(a,b) = " << CONCAT(a,b) << endl; // ## concat the vriable name 'a' and 'b', and the result is ab
+                                                         // so cout << CONCAT(a,b) == cout << ab
+                                                         // 连接的是变量名，而不是字符串
+        cout << endl;
+        
 
+        cout << "=========C++ predefined macro=========" << endl;
+        cout << "Value of __LINE__ : " << __LINE__ << endl;
+        cout << "Value of __FILE__ : " << __FILE__ << endl;
+        cout << "Value of __DATE__ : " << __DATE__ << endl;
+        cout << "Value of __TIME__ : " << __TIME__ << endl;  
+
+    #endif
 
 
 }
